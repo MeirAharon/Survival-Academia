cda6e9d3f60334f384ff1deff178993f73527279
MeirAharon <meir.ahaorn6@gmail.com>
2023-11-29 17:09:46 -0500

    fg

--- loadGame.py
+++ loadGame.py
@@ -1,22 +1,19 @@
 from cmu_graphics import *
 from PIL import Image
-import os, pathlib, time
-from Tile import *
-from Player import *
-import PowerUp
+import pathlib, os
 
 
+
+app.height = 720        
 def onAppStart(app):
-    createMenu(app)
-    createLevel(app)
-    createFrame(app)
-    createPlayer(app)
-    createCollisionBoard(app)
-    app.playState = True
-    app.spriteCounter = 0
-    app.stepsPerSecond = 60
-    app.moving = False
 
+    createMenu(app)
+    creatLevel(app)
+    createCollisionBoard(app)
+    createPlayer(app)
+    createFrame(app)
+    app.playState = True
+    
 def createMenu(app):
 
     app.menuBackground = openImage("assets/menuBackground.png")
@@ -25,94 +22,62 @@ def createMenu(app):
     app.menuBackground = CMUImage(app.menuBackground)
     app.startButton = CMUImage(app.startButton)
 
-def createLevel(app):
-    
+def creatLevel(app):
+     
     app.levelBackground = openImage("assets/sunsetBackgroundLevel.png")
     app.levelBackground = CMUImage(app.levelBackground)
-    app.levelRows = 20
-    app.levelCols = 40
-    app.tileWidth =  70
-    app.tileHeight = app.height // app.levelRows
-    app.level =  []
+    app.levelRows = 0
+    app.levelCols = 0
     app.tileList = []
-    level = open("level1.txt", "r")
+    levelFile = open("level1.txt", "r")
+    for i, row in enumerate(levelFile.readlines()):
+        app.levelRows += 1
+        for j, col in enumerate(row.split(' ')):
+            if col != '-1' and col != '\n' and col != '':
+                x = int(j * Tile.tileWidth)
+                y = int(i * Tile.tileHeight)
+                app.tileList.append(Tile(x, y, int(col)))
+                app.levelCols += 1
+                print(app.height, 'x:', x, 'y:', y, j, i, Tile.tileHeight)
     
-    for i, line in enumerate(level.readlines()):
-        print(level.read())
-        for j ,char in enumerate(line.split(' ')):
-            if char  != '-1' and char != '\n':
-                if char == 3:
-                    x = int(j * app.tileWidth)
-                    y = int(i * app.tileHeight)
-                    app.tileList.append(PowerUp(x, y))
+def createCollisionBoard(app):
+    
+    app.tileDict = dict()
+    for row in range(app.levelRows):
+        for col in range(app.levelCols):
+            app.tileDict[(row, col)] = []       
+    for tile in app.tileList:
+        row = int(tile.y // Tile.tileHeight)
+        col = int(tile.x // Tile.tileWidth)
+        print(tile.y, tile.x)
+        app.tileDict[(row,col)].append(tile)                
 
-                x = int(j * app.tileWidth)
-                y = int(i * app.tileHeight)
-                app.tileList.append(Tile(x, y, int(char)))
-        print(char)
+def createPlayer(app):
+     app.meir = Player(10,0,5)
 
-    # for row in range(app.levelRows):
-    #     if row < app.levelRows - 1:
-    #         app.level.append([-1] * app.levelCols)
-    #     else:
-    #         app.level.append([2] * app.levelCols)
-    # app.level[app.levelRows-2][app.levelCols // 2] = 2 # this is random block for testing
-    # app.rectList = []
-    # app.tileList = []
-    # for row in range(len(app.level)):
-    #     for col in range(len(app.level[row])):
-            
-    #         if app.level[row][col] != -1:
-    #             x = int(col * app.tileWidth)
-    #             y = int(row * app.tileHeight)
-                
-    #             app.tileList.append(Tile(x, y, app.level[row][col]))
-                
 def createFrame(app):
     app.frameRight = False
     app.frameLeft = False
     app.frameMoving = 0
     app.frameSpeed = 5
-  
-def createPlayer(app):
-    app.meir = Player(10, 0, 7)    
 
-def createCollisionBoard(app):
-    
-    app.tileDict = dict()
-    cols = int(app.tileWidth * app.levelCols // Player.width)
-    rows = int(app.height // Player.height)
-    for row in range(rows):
-        for col in range(cols):
-            app.tileDict[(row, col)] = []
-    for tile in app.tileList:
-        row = int(tile.y // Player.height)
-        col = int((tile.x) // Player.width)
-        app.tileDict[(row,col)].append(tile)            
-                  
 def setFrame(app):
     
     if app.frameRight:
         app.frameMoving += app.frameSpeed
     if app.frameLeft and app.frameMoving > 0:
-        app.frameMoving -= app.frameSpeed
+        app.frameMoving -= app.frameSpeed 
 
 def drawMenu(app):
 
     drawImage(app.menuBackground, 0,0)
-    drawImage(app.startButton, app.width//2 - app.startButtonWidth//2, app.height//2)
+    drawImage(app.startButton, app.width//2 - app.startButtonWidth//2, app.height//2)           
 
 def drawLevel(app):
-       
-    for i in range(5):
-        drawImage(app.levelBackground,i * app.width - app.frameMoving, 0)
-    for i in range((app.levelRows)):
-        drawLine(0, app.tileHeight * i, app.width, app.tileHeight * i)
-    for i in range((app.levelCols)):
-        drawLine(app.tileWidth * i, 0, app.tileWidth * i, app.height)     
 
     for tile in app.tileList:
-        drawImage(tile.img, tile.x, tile.y, width=app.tileWidth, height=app.tileHeight, fill = 'yellow')    
+        drawImage(tile.img, tile.x, tile.y, width=Tile.tileWidth, height=Tile.tileHeight, fill = 'yellow')
+
 
 def redrawAll(app):
     
@@ -121,13 +86,14 @@ def redrawAll(app):
     else:
         drawLevel(app)
         app.meir.drawPlayer()   
-        rectWidth, rectHeight = app.meir.playerRectSize[0]  
-        drawLabel(f'v({app.meir.velocity} pixels per second)', app.meir.posX + rectWidth, app.meir.posY + rectHeight + 20, size = 30)
-        
+        # rectWidth, rectHeight = 45, 75  
+        # drawLabel(f'v({app.meir.velocity} pixels per second)', app.meir.posX + rectWidth, app.meir.posY + rectHeight + 20, size = 30)
+
 def onMousePress(app, mouseX, mouseY):
+
     if not app.playState and clickDistance(mouseX, mouseY, app.width//2 - app.startButtonWidth//2,
                                        app.height//2, app.startButtonWidth, app.startButtonHeight):
-        app.playState = True
+        app.playState = True        
 
 def onKeyHold(app, keys):
     app.meir.playerControls(keys, None)
@@ -142,22 +108,215 @@ def onKeyRelease(app, key):
     if key == 'right':
         app.frameRight = False
     elif key == 'left':
-        app.frameLeft = False  
+        app.frameLeft = False
 
 def onStep(app):
     setFrame(app)
     app.meir.updatePlayer() 
     app.meir.calculateVelocity()
     createCollisionBoard(app)
-    
+
 def openImage(fileName):
         return Image.open(os.path.join(pathlib.Path(__file__).parent,fileName))
- 
+
 def clickDistance(mouseX, mouseY, x, y, width, height):
     if (x <= mouseX <= x+width) and (y <= mouseY <= y+height):
         return True 
+
+class Tile():
     
+    print(app.height)
+    tileWidth = 60
+    tileHeight = app.height // 12
+
+    def __init__(self, x, y, imgNum):
+        
+        self.x = x
+        self.y = y
+        self.img = openImage(f"Tiles\\Tile_{imgNum}.png")
+        self.img = CMUImage(self.img)
+
+    
+class Player():
+    
+    def __init__(self, x, y, movementSpeed):
+        self.movementSpeed = movementSpeed
+        self.moving = False
+        self.moveRight = False
+        self.moveLeft = False
+        self.jumping = False
+        self.gravity = True
+        self.jumpHeight = -20
+        self.gravityStrength = 10
+        self.prevPosX = x
+        self.prevPosY = y
+        self.posX = x
+        self.distance =0 #test variable
+        self.posY = y
+        self.futurePosX = 0
+        self.futurePosY = 0
+        self.movementBufferX = 3
+        self.vX = 0
+        self.vY = 0
+        self.gX = 0
+        self.gY = 1
+        self.velocity = 0
+        self.width = 40
+        self.height = 70
+        self.spriteDirection = 'right'
+        self.spriteCounter = 0
+        self.sprites = []
+        self.playerRectSize = []
+        self.spritestrip = openImage("assets/Swordsman_spritelist2.png")
+        self.yCollide = 0
+
+        for i in range(0, 8):
+            sprite = CMUImage(self.spritestrip.crop((45*i, 0, 45+45*i, 75)))
+            self.sprites.append(sprite)
+            self.playerRectSize.append((sprite.image.width, sprite.image.height))
+            self.sprite = self.sprites[0]    
+
+        # self.width, self.height = self.playerRectSize[0]
+
+    def calculateVelocity(self):
+        #this is called from onstep() so time is accounted for 
+        self.vX = (self.posX - self.prevPosX)
+        self.vY = (self.posY - self.prevPosY)
+        self.velocity = ((self.prevPosX - self.posX)**2 + (self.prevPosY - self.posY)**2)**0.5
+        self.prevPosX = self.posX
+        self.prevPosY = self.posY  
+
+    def playerControls(self, keys, key):
+        if 'right' in keys:
+            self.moving = True
+            self.moveRight = True
+        if 'left' in keys:    
+            self.moving = True
+            self.moveLeft = True
+        if 'down' in keys: 
+            self.height  = int(self.height // 2)
+            self.posY = self.posY + self.height
+            self.moving = True
+            self.crouch = True
+        if 'up' in keys:
+            self.moving = True
+            self.jumping = True
+        # checking if player has released the key
+        if 'right' == key:
+            self.moving = False
+            self.moveRight = False
+        if 'left' == key:    
+            self.moving = False
+            self.moveLeft = False
+        if 'down' == key: 
+            self.height = int(self.height*2)
+            self.posY = self.posY + self.height
+            self.moving = False
+            self.crouch = False
+        if 'up' == key:
+            self.moving = False
+            self.jumping = False
+
+    def updatePlayer(self):
+        
+        if Player.checkCollisions(self, 'right'):        
+            if self.moveRight == True:
+                self.spriteCounter = (1 + self.spriteCounter) % len(self.sprites)
+                self.posX += self.movementSpeed
+        if Player.checkCollisions(self, 'left'):        
+            if self.moveLeft == True:
+                self.spriteCounter = (1 + self.spriteCounter) % len(self.sprites)
+                self.posX -= self.movementSpeed   
+        if Player.checkCollisions(self, 'bottom'):
+            if self.gravity == True:
+                self.posY += self.gravityStrength     
+        if Player.checkCollisions(self, 'top'):        
+            if self.jumping == True:
+                self.posY += self.jumpHeight         
+    def getRange(self, row, col):
+        if row == 0 and col == 0:
+            self.rangeStartRows = 0
+            self.rangeEndRows = 2
+            self.rangeStartCols = 0
+            self.rangeEndCols = 2
+        elif row == 0 and col > 0:
+            self.rangeStartRows = 0
+            self.rangeEndRows = 2
+            self.rangeStartCols = -1
+            self.rangeEndCols = 2
+        elif row > 0 and col == 0:
+            self.rangeStartRows = -1
+            self.rangeEndRows = 2
+            self.rangeStartCols = 0
+            self.rangeEndCols = 2
+        elif row > 0 and col > 0:
+            self.rangeStartRows = -1
+            self.rangeEndRows = 2
+            self.rangeStartCols = -1
+            self.rangeEndCols = 2
+            
+    def checkCollisions(self, axis):
+        if axis == 'right' or axis == 'left':
+            
+            Player.futurePosition(self, 'x')
+            col = int(self.futurePosX // self.width)
+            row = int(self.posY // self.height)
+            Player.getRange(self,row,col)
+            for i in range(self.rangeStartRows, self.rangeEndRows):
+                for j in range(self.rangeStartCols, self.rangeEndCols):
+                    for object in app.tileDict[(row + i, col + j)]:
+                        if isinstance(object, Tile):
+                                if Player.rectanglesOverlap(self.futurePosX, self.posY, self.width, self.height, 
+                                                            object.x, object.y, app.tileWidth, app.tileHeight):    
+                                    return False
+            return True    
+        if axis == 'top' or axis == 'bottom':
+            Player.futurePosition(self, 'y')
+            col = int(self.posX // self.width)
+            row = int(self.futurePosY // self.height)
+            Player.getRange(self,row,col)
+
+            for i in range(self.rangeStartRows, self.rangeEndRows):
+                for j in range(self.rangeStartCols, self.rangeEndCols):
+                    for object in app.tileDict[(row + i, col + j)]:
+                        if isinstance(object, Tile):
+                                if Player.rectanglesOverlap(self.posX, self.futurePosY, self.width, self.height, 
+                                                            object.x, object.y, app.tileWidth, app.tileHeight): 
+                                    return False
+            return True    
+    
+    def rectanglesOverlap(left1, top1, width1, height1,
+                            left2, top2, width2, height2):
+        bottom1 = top1 + height1
+        bottom2 = top2 + height2
+        right1 = left1 + width1
+        right2 = left2 + width2
+        return bottom1 >= top2 and bottom2 >= top1 and right1 >= left2 and right2 >= left1
+    def distanceBetweenRects(x1, y1, x2, y2):
+        distance = max(((x2 - x1)/2 , (y2 - y1)/2))
+        print(distance)
+        return distance
+        
+    def futurePosition(self, axis):
+
+        if axis == 'x':        
+            if self.moveRight == True:
+                self.futurePosX = self.posX + self.movementSpeed
+            elif self.moveLeft == True:
+                self.futurePosX = self.posX - self.movementSpeed 
+        
+        elif axis == 'y':
+            if self.gravity == True:
+                self.futurePosY = self.posY + self.gravityStrength
+            if self.jumping == True:
+                self.futurePosY = self.posY + self.jumpHeight + self.gravityStrength    
+
+    def drawPlayer(self):
+        
+        drawImage(self.sprites[self.spriteCounter], self.posX, self.posY)
+        drawRect(self.posX, self.posY, self.width, self.height, fill = None, border = 'black' )    
+
 def main():
-    runApp(width=1280,height=720)    
-    
+    runApp(width = 1280, height = 720)
+
 main()    
